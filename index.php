c48b0657f6d1b0d3f3251375b30b983a94d8ae2f
brunobenchimol <40777698+brunobenchimol@users.noreply.github.com>
2018-07-03 15:04:48 -0300

    Update index.php

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
 if(isset($_POST['ready'])) 
 { 
     echo "<p> <b> Probe de Ready Removido !!! </b> </p>"; 
-    unlink("ready.php");
+    unlink("/ready.php");
 }
 if(isset($_POST['liveness'])) 
 { 
     echo "<p> <b> Probe de Liveness Removido !!! </b> </p>"; 
-    unlink("liveness.php");
+    unlink("/liveness.php");
 }         
           
 ?>
