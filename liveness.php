c1d95de8e4715ac902f6eadcd568c3f21d8b518f
brunobenchimol <40777698+brunobenchimol@users.noreply.github.com>
2018-07-04 15:58:39 -0300

    Update liveness.php

--- liveness.php
+++ liveness.php
@@ -1,5 +1,11 @@
 <?php
 
-  echo "Live Test <BR>";
+$filename = 'alive.txt';
+if (file_exists($filename)) {
+    echo "OK - Im Alive - File exists";
+} else {
+    header("HTTP/1.1 503 Service Unavailable");
+    die("File do Liveness não existe.");
+}
 
 ?>
